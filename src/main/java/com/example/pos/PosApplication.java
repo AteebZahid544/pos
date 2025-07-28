78153403425976d39456fc1ee5f8f7a100ee7ae4
AteebZahid <ateeb.zahid@cba.com.pk>
2025-07-28 19:03:48 +0500

    password encoder

--- src/main/java/com/example/pos/PosApplication.java
+++ src/main/java/com/example/pos/PosApplication.java
@@ -2,7 +2,10 @@ package com.example.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.pos.repo")
@@ -11,5 +14,10 @@ public class PosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
 	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 }
